dd46c29629f0f878eea4b498f68fb9d7d3efb41f
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-06-06 17:50:24 +0000

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+sum of list
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    args: input_list
+    return: sum of list
+    """
+    return sum(input_list)
