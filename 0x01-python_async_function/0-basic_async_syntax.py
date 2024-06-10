fc37762201a4caf388c9c21935b32e8b5fe538af
Anuoluwapo25 <anuoluwapoali25@gmail>
2024-06-10 17:36:25 +0000

    correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-"""
-asynchronous coroutine
-"""
-
-import asyncio
+"""_summary"""
 import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """_summary_
+
+    Args: max_delay
+    Returns:delay
     """
-    args: max_delay
-    return: delay
-    """
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
