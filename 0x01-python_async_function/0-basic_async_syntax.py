9647027688a7feb1d024cfeae2ca6481b9496194
Anuoluwapo25 <anuoluwapoali25@gmail>
2024-06-10 16:32:35 +0000

    pycodestyle correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,12 +4,14 @@ asynchronous coroutine
 """
 
 import asyncio
+import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """
     args: max_delay
-    return: 
+    return: delay
     """
     delay = random.uniform(0, max_delay)
-    await.asyncio.sleep(delay)
-    return delay    
+    await asyncio.sleep(delay)
+    return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """ async routine """
 
+
 import asyncio
 from 0-basic_async_syntax.py import wait_random
 
-async def wait_n(n: int, max_delay: int) -> float :
+
+async def wait_n(n: int, max_delay: int) -> float:
     """
     args: n and max_delay
-    returns: 
+    returns:  tasks
     """
-    task = []:
+    task = []
     for i in range n:
-        task.append(asyncio.create_task(wait_random(max_delay))
-
-    return task 
+        task.append(asyncio.create_task(wait_random(max_delay)))
+    return await asyncio.gather(*tasks)
