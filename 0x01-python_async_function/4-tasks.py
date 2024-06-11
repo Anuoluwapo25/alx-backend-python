255a7d1a08087a7ed44859103305ca99ba94e3ef
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-06-11 00:50:30 +0000

    max_delay

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,10 +8,24 @@ import asyncio
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax')
+task_wait_random = __import__('0-basic_async_syntax').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_random(max_delay: int) -> float:
+"""
+    Simulates a random wait with a maximum delay.
+
+    Args:
+        max_delay (float): The maximum amount of time to wait (in seconds).
+
+    Returns:
+        float: The actual delay used, which will be a random value between 0 and max_delay.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Args: max_delays
     Returns: tasks
