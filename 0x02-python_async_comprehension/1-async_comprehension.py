1e1cf6a324d03458009209ec306b2a4320523b47
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-06-12 02:33:23 +0000

    async genarator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> Generator[float, None, None]:
     """
     Args: lists
     Returns: coroutine list of float
