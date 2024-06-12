349d4cafd9e1808735e44c8655f67e0ec9cae320
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-06-12 04:02:38 +0100

    corrections

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,12 +8,10 @@ from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Args: lists
     Returns: coroutine list of float
     """
-    result: List[float] = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    num_list = [i async for i in async_generator()]
+    return num_list
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,23 +1,15 @@
 """ async_comprehension """
 
 
-import time
 import asyncio
-from datetime import datetime
-from typing import List
+from time import perf_counter
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """
-    Measures the total runtime of executing async_comprehension four times in parallel.
-    
-    Returns:
-        float: The total runtime in seconds.
-    """
-    start = time.perf_counter()
-    await asyncio.gather(*[async_comprehension() for _ in range(4)])
-    end = time.perf_counter()
-    total_run = end - start
-    return total_run
+    """Run time for four parallel comprehensions"""
+    start = perf_counter()
+    numbers_list = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*numbers_list)
+    return perf_counter() - start
