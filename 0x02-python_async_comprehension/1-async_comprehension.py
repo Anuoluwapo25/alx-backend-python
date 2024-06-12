09036747c8af1aa3ffffa97ce4aca00281eb5211
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-06-12 23:07:37 +0000

    updating deleted file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Args: lists
     Returns: coroutine list of float
